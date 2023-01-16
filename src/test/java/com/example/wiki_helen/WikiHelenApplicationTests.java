702254eb9a0aa9ca55ee363f633f7351d64e5f51
zhaowrhelen <zhaowrhelen@gmail.com>
2023-01-15 22:55:20 -0800

    Initial commit

---
+++ src/test/java/com/example/wiki_helen/WikiHelenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wiki_helen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiHelenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
