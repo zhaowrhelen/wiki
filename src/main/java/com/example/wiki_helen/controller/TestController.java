a5540c113d611b36d4b5b7670ad496afc267fa95
zhaowrhelen <58273143+zhaowrhelen@users.noreply.github.com>
2023-03-23 17:19:51 -0700

    http test file

--- src/main/java/com/example/wiki_helen/controller/TestController.java
+++ src/main/java/com/example/wiki_helen/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.wiki_helen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World,post " + name;
     }
 }
