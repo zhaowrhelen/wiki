902c6564656ed2938d372b4dccbbabea51f5e0f2
zhaowrhelen <58273143+zhaowrhelen@users.noreply.github.com>
2023-03-23 17:57:57 -0700

    customize value

--- src/main/java/com/example/wiki_helen/controller/TestController.java
+++ src/main/java/com/example/wiki_helen/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki_helen.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World" + testHello;
     }
 
     @PostMapping("/hello/post")
