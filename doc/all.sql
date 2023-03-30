3f1a29f744e065f29e0e7a9b21aab4faf14e0f57
wanrongzhao <zhaowrhelen@gmail.com>
2023-03-30 16:50:20 -0700

    mybatis generator

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,13 @@ create table `test` (
 ) engine=innodb default charset=utf8mb4 comment='test';
 
 insert into `test` (id, name, password) VALUES (1, 'test', '123');
+
+
+drop table if exists `demo`;
+create table `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key(`id`)
+) engine = innodb default charset = utf8mb4 comment = 'test';
+
+insert into `demo` (id,name) values(1,'test');
