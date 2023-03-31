46f1f7a782790c4a8a345aa5bd56c08f96193c3f
wanrongzhao <zhaowrhelen@gmail.com>
2023-03-30 17:50:05 -0700

    ebook list search interface

---
+++ src/main/java/com/example/wiki_helen/controller/EbookController.java
@@ -0,0 +1,27 @@
+package com.example.wiki_helen.controller;
+
+
+import com.example.wiki_helen.domain.Ebook;
+
+import com.example.wiki_helen.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/example/wiki_helen/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.example.wiki_helen.service;
+
+import com.example.wiki_helen.domain.Ebook;
+import com.example.wiki_helen.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
