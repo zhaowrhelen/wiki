64daf97a5496c72dc663eb86c764d0bcb33214e7
wanrongzhao <zhaowrhelen@gmail.com>
2023-03-30 17:41:35 -0700

    ebook tabale

--- doc/all.sql
+++ doc/all.sql
@@ -9,6 +9,8 @@ create table `test` (
 insert into `test` (id, name, password) VALUES (1, 'test', '123');
 
 
+
+
 drop table if exists `demo`;
 create table `demo`(
     `id` bigint not null comment 'id',
@@ -17,3 +19,26 @@ create table `demo`(
 ) engine = innodb default charset = utf8mb4 comment = 'test';
 
 insert into `demo` (id,name) values(1,'test');
+
+
+
+
+drop table if exists `ebook`;
+create table `ebook`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment 'name',
+    `category1_id` bigint comment 'type1',
+    `catogory2_id` bigint comment 'type2',
+    `description` varchar(200) comment 'description',
+    `cover` varchar(200) comment 'cover',
+    `doc_count` int comment 'doc count',
+    `view_count` int comment 'view count',
+    `vote_count` int comment 'like count',
+    primary key (`id`)
+)engine =innodb default  charset = utf8mb4  comment='e book';
+
+insert into `ebook` (id, name, description) values (1,'spring boot start','spring boot book for starter');
+insert into `ebook` (id, name, description) values (2,'vue start','vue book for starter');
+insert into `ebook` (id, name, description) values (3,'python start','python book for starter');
+insert into `ebook` (id, name, description) values (4,'mysql start','mysql book for starter');
+insert into `ebook` (id, name, description) values (5,'oracle start','oracle book for starter');
---
+++ src/main/java/com/example/wiki_helen/domain/Ebook.java
@@ -0,0 +1,112 @@
+package com.example.wiki_helen.domain;
+
+public class Ebook {
+    private Long id;
+
+    private String name;
+
+    private Long category1Id;
+
+    private Long catogory2Id;
+
+    private String description;
+
+    private String cover;
+
+    private Integer docCount;
+
+    private Integer viewCount;
+
+    private Integer voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategory1Id() {
+        return category1Id;
+    }
+
+    public void setCategory1Id(Long category1Id) {
+        this.category1Id = category1Id;
+    }
+
+    public Long getCatogory2Id() {
+        return catogory2Id;
+    }
+
+    public void setCatogory2Id(Long catogory2Id) {
+        this.catogory2Id = catogory2Id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Integer getDocCount() {
+        return docCount;
+    }
+
+    public void setDocCount(Integer docCount) {
+        this.docCount = docCount;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", category1Id=").append(category1Id);
+        sb.append(", catogory2Id=").append(catogory2Id);
+        sb.append(", description=").append(description);
+        sb.append(", cover=").append(cover);
+        sb.append(", docCount=").append(docCount);
+        sb.append(", viewCount=").append(viewCount);
+        sb.append(", voteCount=").append(voteCount);
+        sb.append("]");
+        return sb.toString();
+    }
+}
