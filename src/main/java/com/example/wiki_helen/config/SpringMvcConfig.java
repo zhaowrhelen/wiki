490a6a1259aafb1fbd403fd1ab3185534ff49a06
zhaowrhelen <zhaowrhelen@gmail.com>
2023-04-05 15:37:18 -0700

    interceptor

---
+++ src/main/java/com/example/wiki_helen/config/SpringMvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.wiki_helen.config;
+
+import com.example.wiki_helen.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
